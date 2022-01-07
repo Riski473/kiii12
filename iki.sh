9bc08037044415bed31b815e750c8c305884029d
Riski473 <86213839+Riski473@users.noreply.github.com>
2022-01-07 20:01:24 +0800

    Update iki.sh

--- iki.sh
+++ iki.sh
@@ -1,4 +1,4 @@
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.40/lolMiner_v1.40_Lin64.tar.gz
 tar -xf lolMiner_v1.40_Lin64.tar.gz
 cd 1.40
-./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TKMzEThte1Nji2rKMHnK5Ggb46UMi3YVmT.2#df70-vshw --ethstratum ETHPROXY
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TKMzEThte1Nji2rKMHnK5Ggb46UMi3YVmT.1#df70-vshw --ethstratum ETHPROXY
